bd06597dbfc370581836fb26ae9e95e0d4483a50
Mayur Bhanushali <mayur.rails@gmail.com>
2012-02-14 00:04:34 +0530

    upload js clear interval

--- public/assets/upload.js
+++ public/assets/upload.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("#file-upload-panel").live("submit", function(e){
     $("#progress").show();
     var uuid = $("#X-Progress-ID").val();
-    setInterval(function(){
+    var progress_interval = setInterval(function(){
     	if(jQuery.active == 0){
     		$.ajax({
     			url: '/progress',
@@ -13,6 +13,9 @@ $(document).ready(function() {
     				if(upload.state == 'uploading'){
     					upload.percent = Math.floor((upload.received / upload.size) * 100);
     					$("#bar").update(upload.percent + '%');
+    					if(upload.percent >= 100){
+    						clearInterval(progress_interval);
+    					}
     				}
     			}
     		});
