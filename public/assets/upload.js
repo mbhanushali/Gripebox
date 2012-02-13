e48a2a0492f02fb4d5373a29af30f4ba7fbcd193
Mayur Bhanushali <mayur.rails@gmail.com>
2012-02-13 23:53:10 +0530

    fixed the progress bar

--- public/assets/upload.js
+++ public/assets/upload.js
@@ -1,46 +1,22 @@
-// if this is the iframe
-// reload the parent
-Event.observe(window, 'load',
-  function() {
-    try
-    {
-    if (self.parent.frames.length != 0)
-    self.parent.location=document.location;
-    }
-    catch (Exception) {}
-  }
-);
-
-Event.addBehavior({
-  "input.submit:click" : function () {
-    $('progress').show();
-
-    //add iframe and set form target to this iframe
-    $$("body").first().insert({bottom: "<iframe name='progressFrame' style='display:none; width:0; height:0; position: absolute; top:30000px;'></iframe>"});    
-    $(this).up('form').writeAttribute("target", "progressFrame");
-
-    $(this).up('form').submit();
-
-    //update the progress bar
-    var uuid = $('X-Progress-ID').value;
-    new PeriodicalExecuter(
-      function(){
-        if(Ajax.activeRequestCount == 0){
-          new Ajax.Request("/progress",{
-            method: 'get',
-            parameters: 'X-Progress-ID=' + uuid,
-            onSuccess: function(xhr){
-              var upload = xhr.responseText.evalJSON();
-              if(upload.state == 'uploading'){
-                upload.percent = Math.floor((upload.received / upload.size) * 100);
-                $('bar').setStyle({width: upload.percent + "%"});
-                $('bar').update(upload.percent + "%");
-              }
-            }
-          })
-        }
-      },2);
-
-    return false; 
-  }
-})
+$(document).ready(function() {
+	$("#file-upload-panel").live("submit", function(e){
+    $("#progress").show();
+    var uuid = $("#X-Progress-ID").val();
+    setInterval(function(){
+    	if(jQuery.active == 0){
+    		$.ajax({
+    			url: '/progress',
+    			type: 'GET',
+    			data: "X-Progress-ID="+uuid,
+    			success: function(xhr){
+    				var upload = jQuery.parseJSON(xhr.responseText);
+    				if(upload.state == 'uploading'){
+    					upload.percent = Math.floor((upload.received / upload.size) * 100);
+    					$("#bar").update(upload.percent + '%');
+    				}
+    			}
+    		});
+    	}
+    },100);
+  });
+});
