44a94fabd012f9618bada28fc9efef8c6cdbfc31
dbhanushali <dipen.bhanushali@gmail.com>
2012-02-14 17:04:28 +0530

    upload progress bar

--- public/assets/upload.js
+++ public/assets/upload.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$("#file-upload-panel").live("submit", function(e){
-    $("#progress").show();
+    var block_edit_image = $(".block-edit-image:first .progress");
+    $("#progressbar").progressbar();
+    block_edit_image.progressbar();
     var uuid = $("#X-Progress-ID").val();
     var progress_interval = setInterval(function(){
     	if(jQuery.active == 0){
@@ -11,11 +13,13 @@ $(document).ready(function() {
     			success: function(xhr){
     				if(xhr.state == 'uploading'){
     					xhr.percent = Math.floor((xhr.received / xhr.size) * 100);
-    					$("#bar").html(xhr.percent + '%');
+    					block_edit_image.progressbar("option","value",xhr.percent);
+    					$("#progressbar").progressbar("option","value",xhr.percent);
     				}
     				else if(xhr.state == 'done'){
     					clearInterval(progress_interval);
-    					$("#bar").html('100%');
+    					block_edit_image.progressbar("option","value",100);
+    					$("#progressbar").progressbar("option","value",xhr.percent);
     				}
     			}
     		});
