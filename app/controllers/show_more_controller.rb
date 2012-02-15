1c26920dbdb5a5295bebb6c58b4e869d47b71756
Mayur Bhanushali <mayur.rails@gmail.com>
2012-02-15 22:12:46 +0530

    default gripes set to 10 on home page

--- app/controllers/show_more_controller.rb
+++ app/controllers/show_more_controller.rb
@@ -1,7 +1,7 @@
 class ShowMoreController < ApplicationController
   def index
     @page = params[:page]
-    @gripes = Gripe.where('created_at > ?',1.month.ago).order(:power).order("comments_count DESC").order("created_at DESC").paginate(:page => @page, :per_page => 5)
+    @gripes = Gripe.where('created_at > ?',1.month.ago).order(:power).order("comments_count DESC").order("created_at DESC").paginate(:page => @page, :per_page => 10)
     if @gripes.count > 0
       render :partial => "gripe", :collection => @gripes
     else
