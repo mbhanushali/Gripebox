93b2cf2da444a378b491918909dababcf662733b
Mayur Bhanushali <mayur.rails@gmail.com>
2012-02-20 23:34:22 +0530

    fixed the facebook and twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 #  if Rails.env == 'production'
-    provider :twitter, '2KM9g9xheM0IOClkWioxw', 'vchl69mJPLQxa6rYXUidq3nLJyHDvNNW0RHyez4gA'
-    provider :facebook, '292822210738107', '9822dc415bf97f509ba41c689628a906'
+    provider :twitter, 'ynZ9AyufbSUNct5sag74iw', '1qucTtFnxKGaeAFaOtAGx64KWLe3Dqa5nEVwGItnb0'
+    provider :facebook, '259665494110245', '8c55ada2517263f7d88f0f53a4aa6744'
 #  else
 #    provider :twitter, 'aYSqTba7BZ340ZiC6nXULA', '7FzoCFhIAwvVBPvb1Pk1gqGMU21Z4sRJ8hdeVw1U'
 #    provider :facebook, '309309782430004', '0e74f3175a3995c79653dbe18c60d0d8'
