cfe9198082250d478cf8d4e09ef4188ca30070cc
Mayur Bhanushali <mayur.rails@gmail.com>
2012-02-24 01:54:40 +0530

    fixing issue with exception notification

--- config.ru
+++ config.ru
@@ -2,9 +2,3 @@
 
 require ::File.expand_path('../config/environment',  __FILE__)
 run Gripebox::Application
-
-Gripebox::Application.config.middleware.use ExceptionNotifier,
- :email_prefix => "[Gripebox Exception] ",
- :sender_address => %{"notifier" <support@gripebox.com>},
- :exception_recipients => %w{mayur.rails@gmail.com},
- :sections => %w{my_section1 my_section2} + ExceptionNotifier::Notifier.default_sections
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,9 @@ require File.expand_path('../application', __FILE__)
 Gripebox::Application.initialize!
 
 Haml::Template.options[:format] = :html5
+
+Gripebox::Application.config.middleware.use ExceptionNotifier,
+ :email_prefix => "[Gripebox Exception] ",
+ :sender_address => %{"notifier" <support@gripebox.com>},
+ :exception_recipients => %w{mayur.rails@gmail.com},
+ :sections => %w{my_section1 my_section2} + ExceptionNotifier::Notifier.default_sections
